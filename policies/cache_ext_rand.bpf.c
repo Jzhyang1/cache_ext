6bd2b16d45b7fee158777b52b7f563de6726916b
jzhyang1 <jzhyang1@gmail.com>
2025-11-24 20:30:25 -0600

    random replacement policy

---
+++ policies/cache_ext_rand.bpf.c
@@ -0,0 +1,71 @@
+#include "vmlinux.h"
+#include <bpf/bpf_helpers.h>
+#include <bpf/bpf_tracing.h>
+#include <bpf/bpf_core_read.h>
+
+#include "cache_ext_lib.bpf.h"
+#include "dir_watcher.bpf.h"
+
+char _license[] SEC("license") = "GPL";
+
+static u64 main_list;
+
+static inline bool is_folio_relevant(struct folio *folio) {
+	if (!folio || !folio->mapping || !folio->mapping->host)
+		return false;
+
+	return inode_in_watchlist(folio->mapping->host->i_ino);
+}
+
+s32 BPF_STRUCT_OPS_SLEEPABLE(rand_init, struct mem_cgroup *memcg)
+{
+	main_list = bpf_cache_ext_ds_registry_new_list(memcg);
+	if (main_list == 0) {
+		bpf_printk("cache_ext: init: Failed to create main_list\n");
+		return -1;
+	}
+	bpf_printk("cache_ext: Created main_list: %llu\n", main_list);
+
+	return 0;
+}
+
+static int bpf_rand_evict_cb(int idx, struct cache_ext_list_node *a)
+{
+	u32 rnum = bpf_get_prandom_u32();
+	if ((rnum ^ idx) & 0x00000FFF) return CACHE_EXT_EVICT_NODE;
+	return CACHE_EXT_CONTINUE_ITER;
+}
+
+void BPF_STRUCT_OPS(rand_evict_folios, struct cache_ext_eviction_ctx *eviction_ctx,
+		    struct mem_cgroup *memcg)
+{
+	if (bpf_cache_ext_list_iterate(memcg, main_list, bpf_rand_evict_cb, eviction_ctx) < 0) {
+		bpf_printk("cache_ext: evict: Failed to iterate main_list\n");
+		return;
+	}
+}
+
+void BPF_STRUCT_OPS(rand_folio_evicted, struct folio *folio) {
+	// if (bpf_cache_ext_list_del(folio)) {
+	// 	bpf_printk("cache_ext: Failed to delete folio from list\n");
+	// 	return;
+	// }
+}
+
+void BPF_STRUCT_OPS(rand_folio_added, struct folio *folio) {
+	if (!is_folio_relevant(folio))
+		return;
+
+	if (bpf_cache_ext_list_add_tail(main_list, folio)) {
+		bpf_printk("cache_ext: added: Failed to add folio to main_list\n");
+		return;
+	}
+}
+
+SEC(".struct_ops.link")
+struct cache_ext_ops rand_ops = {
+	.init = (void *)rand_init,
+	.evict_folios = (void *)rand_evict_folios,
+	.folio_evicted = (void *)rand_folio_evicted,
+	.folio_added = (void *)rand_folio_added,
+};
