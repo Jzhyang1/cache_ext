d7016ddfcf5bda8e17c25751871281d0b199b952
jzhyang1 <jzhyang1@gmail.com>
2026-04-20 10:19:39 -0500

    new test trace

--- analysis/generate_traces.py
+++ analysis/generate_traces.py
@@ -1,16 +1,18 @@
-def log_entry_to_bytes(nr_event, type, drop_count, address_space, page_index):
+def log_entry_to_bytes(nr_event, type, drop_count, address_space, page_index, pid_self, pid_next):
     return (nr_event.to_bytes(8, 'little') +
             type.to_bytes(4, 'little') +
             drop_count.to_bytes(4, 'little') +
             address_space.to_bytes(8, 'little') +
-            page_index.to_bytes(8, 'little'))
+            page_index.to_bytes(8, 'little') +
+            pid_self.to_bytes(4, 'little') +
+            pid_next.to_bytes(4, 'little'))
 
-with open('ref1.log', 'wb') as f:
+with open('ref2.log', 'wb') as f:
     for i in range(200):
-        entry_bytes = log_entry_to_bytes(i, 0, 0, 0, i)
+        entry_bytes = log_entry_to_bytes(i, 0, 0, 0, i, 1, 1)
         f.write(entry_bytes)
         
-with open('pred1.log', 'wb') as f:
+with open('pred2.log', 'wb') as f:
     for i in range(200):
-        entry_bytes = log_entry_to_bytes(i, 0, 0, 0, i)
+        entry_bytes = log_entry_to_bytes(i, 0, 0, 0, i, 1, 1)
         f.write(entry_bytes)
